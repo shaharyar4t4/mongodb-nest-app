bcb3ffdd707e1e38d30f1fded006cab16ab6104a
shaharyarali <shaharyar4t4@gmail.com>
2026-01-29 20:16:40 +0500

    learn about mongoose (complete)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+
+   // this below line for config it help to access the data which is mention of .evn file
+   ConfigModule.forRoot(),
+   // this below line is help to implemented the mongoose (mongo db) in whole project
+   MongooseModule.forRoot(process.env.MONGO_URI!)
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
