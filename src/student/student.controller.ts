62919cfc8b3f91be3846b35fe1ac81286258b77c
shaharyarali <shaharyar4t4@gmail.com>
2026-02-08 00:01:30 +0500

    learn about the get method ( Find() and findbyID())

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.schema';
 
@@ -11,5 +11,16 @@ export class StudentController {
     @Post()
     async addStudent(@Body() data: Partial<Student>){
         return this.studentService.createStudent(data);
+    }
+
+    // this is get controller which is help to fetch All data... 
+    @Get()
+    async getStudent(){
+        return this.studentService.getAllStudent();
+    }
+
+    @Get(':id')
+    async getStudentbyId(@Param('id') id: string){
+        return this.studentService.getStudentbyId(id);
     }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -5,18 +5,35 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class StudentService {
-    
-    constructor(
-            // this is a injectable model --> is ka kam ye hota ha kiya jo schema bnta ha is ko ye sevice ki file me resigeter karvata ha 
-        @InjectModel(Student.name) private StudentModel: Model<StudentDocument>
-    ){}
-   
-    // the keyword "Partial" is show that the --->  some data is optional
-  async createStudent (data: Partial<Student>): Promise<Student>{
-   
+
+  constructor(
+    // this is a injectable model --> is ka kam ye hota ha kiya jo schema bnta ha is ko ye sevice ki file me resigeter karvata ha 
+    @InjectModel(Student.name) private StudentModel: Model<StudentDocument>
+  ) { }
+
+  // POST method (Create the data on DB)
+  // the keyword "Partial" is show that the --->  some data is optional
+  async createStudent(data: Partial<Student>): Promise<Student> {
+
     // the newStudent is help to add the data on DB
     const newStudent = new this.StudentModel(data);
     // this save method on DB
     return newStudent.save();
   }
+
+  // GET method (Get the data on DB)
+  //Promise is show that the data is geted... but your another will be continous..
+  async getAllStudent(): Promise<Student[]>{
+    // the find() is help to get data from data base...
+    // exec is help to fetch data with easliy...
+    return this.StudentModel.find().exec();
+  }
+
+  // GET method (Get the data on DB)
+  // the method help to fetch data by using specfic id...
+  async getStudentbyId(id: string): Promise<Student | null>{
+    // the findByID() is help to get specfic data from data base...
+    return this.StudentModel.findById(id).exec();
+  }
+
 }
