49575a6853797159f7742ae83fce3be3c97dfc71
shaharyar <shaharyar4t4@gmail.com>
2026-02-10 12:04:26 +0500

    complete the learn about one to one Relationship

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schemas/user.schemas';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+    // this is POST method is help to add the data on database 
+    @Post()
+    // start is me na async remove --->  is ki need ki waja ye ha data tu constant aarha ha 
+
+    create(@Body() data: Partial<User>) {
+        return this.userService.createUser();
+    }
+
+    @Get()
+    getAll() {
+        return this.userService.findAll();
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.schemas';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectModel(User.name) private UserModel: Model<User>){}
+
+    // create a sevice for insecting the data on mongoo db
+    async createUser(): Promise<User>{
+        const user = new this.UserModel({
+            // this is basic structure of schema which is insect on Mongoo db
+            name: 'Shaharyar',
+            address: {
+                street: '123 street',
+                city: 'Karachi'
+            }
+        })
+        // save function is help to save the data in mongo db
+        return user.save();
+    }
+
+    // seraching query 
+    async findAll(): Promise<User[]>{
+        return this.UserModel.find();
+    }
+}
